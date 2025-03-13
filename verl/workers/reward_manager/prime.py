7b7932e3ac4137bca0fd40075f0a1c3bd3151560
Yizhi Li <yizhi.li@hotmail.com>
2025-03-13 10:47:40 +0000

    [fix] extra_info passing in prime reward manager

--- verl/workers/reward_manager/prime.py
+++ verl/workers/reward_manager/prime.py
@@ -102,7 +102,8 @@ class PrimeRewardManager:
         sequences_str = self.tokenizer.batch_decode(response_ids, skip_special_tokens=True)
         ground_truth = [data_item.non_tensor_batch['reward_model']['ground_truth'] for data_item in data]
         data_sources = data.non_tensor_batch['data_source']
-
+        extra_info = [data_item.non_tensor_batch.get('extra_info', None) for data_item in data]
+        
         assert len(sequences_str) == len(ground_truth) == len(data_sources)
         try:
             scores = asyncio.run(
